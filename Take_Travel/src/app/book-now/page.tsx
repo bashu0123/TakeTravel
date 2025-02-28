bdc8c4c5e63f65bf32f893e43737d88215467844
adarniya <anishkc10245@gmail.com>
2025-02-28 18:35:52 +0545

    add book now page

---
+++ Take_Travel/src/app/book-now/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Booking from '@/components/template/booking/Booking';
+
+const BookingPage = () => {
+  return (
+    <div className="min-h-screen">
+      
+      <Booking />
+    </div>
+  );
+};
+
+export default BookingPage;
