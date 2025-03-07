d2418eb425a2c6775f6bfac0fdbfc2dd13590733
anivamhj <anivamhj444@gmail.com>
2025-03-07 17:30:28 +0545

    use toaster component and utils

---
+++ Take_Travel/src/lib/utils.ts
@@ -0,0 +1,27 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const convertImageToBase64 = (file:File) => {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject(new Error('No file provided'));
+      return;
+    }
+    
+    const reader = new FileReader();
+    
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    
+    reader.onerror = (error) => {
+      reject(error);
+    };
+    
+    reader.readAsDataURL(file);
+  });
+};
